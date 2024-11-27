ce6b5b889c48e8b86f608edc1c75aa168d582669
abv-v5 <vabhay22435@gmail.com>
2024-11-27 18:23:25 +0530

    again edited question 2 of string

--- coding-assignment/string/string_q02.py
+++ coding-assignment/string/string_q02.py
@@ -1,9 +1,9 @@
 # program to find all index values of a given character in a given string
-st = 'python programming op'
+str = 'python programming op'
 ch = 'o'
-cc = st.count(ch)
+cc = str.count(ch)
 t = 0
 for i in range(cc):
-    a = st.index(ch, t)
+    a = str.index(ch, t)
     print(a)
     t = a + 1
